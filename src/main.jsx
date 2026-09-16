9c580bc9759b413bb0533253ecdcd090a669afab
Weparche <154843953+Weparche@users.noreply.github.com>
2026-09-17 00:44:07 +0200

    fix: keep consent controls active on guide routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MotionConfig } from "framer-motion";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import ConsentManager from "./ConsentManager.jsx";
 import DigitalPriceListGuidePage, { isDigitalPriceListGuidePath } from "./DigitalPriceListGuidePage.jsx";
 import "@fontsource-variable/inter";
 import "./styles.css";
@@ -14,7 +16,12 @@ const pathname = window.location.pathname !== "/"
   ? window.location.pathname.replace(/\/+$/, "")
   : "/";
 const page = isDigitalPriceListGuidePath(pathname)
-  ? <DigitalPriceListGuidePage routePath={pathname} />
+  ? (
+    <BrowserRouter>
+      <DigitalPriceListGuidePage routePath={pathname} />
+      <ConsentManager />
+    </BrowserRouter>
+  )
   : <App />;
 
 createRoot(document.getElementById("root")).render(
